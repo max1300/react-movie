8713f4ceb851237d060650214490516c0bc9692a
Maxime RENAUD <mre@cnam-lyon.peda>
2019-11-29 15:10:13 +0100

    fourth commit, 29112019

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.scss';
 import Navbar from "./Navbar";
 import AddMovie from "./AddMovie";
-import Movies from "./Movies";
+import DisplayMovies from "./DisplayMovies";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ContactForm from "./ContactForm";
 import FirstPage from "./FirstPage";
@@ -18,7 +18,7 @@ const App = () => {
                       <Route exact path="/" component={FirstPage} />
                       <Route path="/contact" component={ContactForm} />
                       <Route path="/addmovie" component={AddMovie} />
-                      <Route path="/movies" component={Movies} />
+                      <Route path="/movies" component={DisplayMovies} />
                   </Switch>
               </div>
           </Router>
