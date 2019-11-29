4177c1f20944852438f2d96883b61a724012f3f8
Lucas Delobelle <lucas@ld-web.net>
2019-11-29 15:51:00 +0100

    Moved Navbar to components

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.scss';
-import Navbar from "../Navbar";
+import React from "react";
+import "./App.scss";
+import Navbar from "./Navbar";
 import AddMovie from "../AddMovie";
 import DisplayMovies from "../DisplayMovies";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -8,22 +8,21 @@ import ContactForm from "../ContactForm";
 import Home from "./Home";
 
 const App = () => {
-
   return (
     <>
-      <Navbar/>
-          <Router>
-              <div>
-                  <Switch>
-                      <Route path="/contact" component={ContactForm} />
-                      <Route path="/addmovie" component={AddMovie} />
-                      <Route path="/movies" component={DisplayMovies} />
-                      <Route exact path="/" component={Home} />
-                  </Switch>
-              </div>
-          </Router>
+      <Navbar />
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/contact" component={ContactForm} />
+            <Route path="/addmovie" component={AddMovie} />
+            <Route path="/movies" component={DisplayMovies} />
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </div>
+      </Router>
     </>
   );
-}
+};
 
 export default App;
